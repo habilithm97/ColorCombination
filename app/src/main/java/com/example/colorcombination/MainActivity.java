595e98414066707a03bd9d1d569bdd2829aa5810
habilithm97 <habinkim97@gmail.com>
2022-05-23 13:34:50 +0900

    메인 UI 수정 및 입력창 글자 수 제한

--- app/src/main/java/com/example/colorcombination/MainActivity.java
+++ app/src/main/java/com/example/colorcombination/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.colorcombination;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView main = (TextView)findViewById(R.id.main);
+        EditText edt = (EditText)findViewById(R.id.edt);
+        EditText edt2 = (EditText)findViewById(R.id.edt2);
+        EditText edt3 = (EditText)findViewById(R.id.edt3);
+        EditText edt4 = (EditText)findViewById(R.id.edt4);
+        Button btn = (Button)findViewById(R.id.btn);
     }
 }
